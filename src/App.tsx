0d5a3cb0f457838ddca61e08b6a5c2134de23754
keen <kyh0927@within.co.kr>
2019-10-10 12:42:06 +0900

    191010

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,23 @@
 import React from 'react';
-import Greetings from './Greetings';
-
-
+import MyForm from './Myform';
+// import Counter from './Counter';
+// import Greetings from './Greetings';
 
 const App: React.FC = () => {
 
-  const onClick = (name: string) => {
-    console.log(`${name} says hello`);
-  }
+  // const onClick = (name: string) => {
+  //   console.log(`${name} says hello`);
+  // }
+
+  const onSubmit = (form: {name: string; description: string}) => {
+    console.log(form);
+  };
 
   return (
     <div>
-      <Greetings name="keen" optional="호잇" onClick={onClick} />
+      <MyForm onSubmit={onSubmit} />
+      {/* <Counter /> */}
+      {/* <Greetings name="keen" optional="호잇" onClick={onClick} /> */}
     </div>
   );
 }
