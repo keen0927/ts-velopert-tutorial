1bc0280f176cefce7bc5941d6369faa93441541c
KEEN <kyh0927@live.co.kr>
2019-10-18 01:06:35 +0900

    191018

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,10 @@
 import React from 'react';
-import Greetings from './Greetings';
-
-
 
 const App: React.FC = () => {
 
-  const onClick = (name: string) => {
-    console.log(`${name} says hello`);
-  }
-
   return (
     <div>
-      <Greetings name="keen" optional="호잇" onClick={onClick} />
+      HI
     </div>
   );
 }
