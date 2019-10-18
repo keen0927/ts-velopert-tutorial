4f8748ea24ff010958614ddef37815e8ceaab0e8
keen <kyh0927@within.co.kr>
2019-10-18 09:57:32 +0900

    191018

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,11 @@
 import React from 'react';
-<<<<<<< HEAD
+import CounterContainer from './containers/CounterContainer';
 
-const App: React.FC = () => {
 
+function App() {
   return (
     <div>
-      HI
-=======
-import MyForm from './Myform';
-// import Counter from './Counter';
-// import Greetings from './Greetings';
-
-const App: React.FC = () => {
-
-  // const onClick = (name: string) => {
-  //   console.log(`${name} says hello`);
-  // }
-
-  const onSubmit = (form: {name: string; description: string}) => {
-    console.log(form);
-  };
-
-  return (
-    <div>
-      <MyForm onSubmit={onSubmit} />
-      {/* <Counter /> */}
-      {/* <Greetings name="keen" optional="호잇" onClick={onClick} /> */}
->>>>>>> 0d5a3cb0f457838ddca61e08b6a5c2134de23754
+      <CounterContainer />
     </div>
   );
 }
