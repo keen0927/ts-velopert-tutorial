54b70ac16980f3b8cfd418236e122e24beadfb09
KEEN <kyh0927@live.co.kr>
2019-10-24 00:00:43 +0900

    191024

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
-import Counter from './components/Counter';
+import TodoInsert from './components/TodoInsert';
+import TodoList from './components/TodoList';
+// import Counter from './components/Counter';
 
 
 function App() {
   return (
     <div>
-      <Counter />
+      {/*<Counter />*/}
+      <TodoInsert />
+      <TodoList />
     </div>
   );
 }
