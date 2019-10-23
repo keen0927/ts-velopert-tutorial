e0274a777ee359cda7e8a1b2424c60d6ec2e6860
KEEN <kyh0927@live.co.kr>
2019-10-23 23:28:28 +0900

    191023

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import CounterContainer from './containers/CounterContainer';
+import Counter from './components/Counter';
 
 
 function App() {
   return (
     <div>
-      <CounterContainer />
+      <Counter />
     </div>
   );
 }
--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -27,20 +27,14 @@ import useCounter from '../hooks/useCounter';
 function Counter() {
   const { count, onIncrease, onDecrease, onIncreaseBy } = useCounter();
 
-  const handleDecrease = () => {
-    if (count === 0) return;
-    onDecrease();
-  }
-
   return (
     <div>
       <h1>{count}</h1>
       <button onClick={onIncrease}>+1</button>
-      {/* <button onClick={handleDecrease}>-1</button> */}
       <button onClick={() => count === 0 ? '' : onDecrease()}>-1</button>
-      {/* <button onClick={handleDecrease}>-1</button> */}
       <button onClick={() => onIncreaseBy(5)}>+5</button>
-
+      {/* <button onClick={handleDecrease}>-1</button> */}
+      {/* <button onClick={handleDecrease}>-1</button> */}
     </div>
   )
 }
