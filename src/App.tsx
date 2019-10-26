68ab6a7cedf89cb477cd846b3820dc219143334b
KEEN <kyh0927@live.co.kr>
2019-10-26 22:14:15 +0900

    191026

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import TodoInsert from './components/TodoInsert';
-import TodoList from './components/TodoList';
-// import Counter from './components/Counter';
+// import TodoInsert from './components/TodoInsert';
+// import TodoList from './components/TodoList';
+import Counter from './components/Counter';
 
 
 function App() {
   return (
     <div>
-      {/*<Counter />*/}
-      <TodoInsert />
-      <TodoList />
+      <Counter />
+      {/*<TodoInsert />*/}
+      {/*<TodoList />*/}
     </div>
   );
 }
